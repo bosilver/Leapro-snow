2710cc294b356701792f91d647564065f4defd3f
Sherry Lin <sherry7737@gmail.com>
2015-05-05 19:58:49 +0800

    using split ";" instead of "&"

--- leapro_snow/static/js/profile.js
+++ leapro_snow/static/js/profile.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
     $('img #profile-img').css({'width':'550px', 'heigh':'550px'})
 
-    var profile = description.split('&')
+    var profile = description.split(';')
     for (var i = 0; i < profile.length; i++) {
         if (i == 0) {
             $('#profile-img').attr('src', profile[i])
