5c9042b66c0fa5b4529a4106cd39d6ddcddd1704
Sherry Lin <sherry7737@gmail.com>
2016-06-22 14:21:57 +0800

    Modify build script. Add Larry, Wayne, Phil, Linlin data.

--- leapro_snow/build.sh
+++ leapro_snow/build.sh
@@ -2,6 +2,9 @@
 rm -rf build
 
 python manage.py staticsitegen --settings=leapro_snow.medusa_settings
+cp -r build/* /Users/sherry/Documents/program-project/website/leaprosnow-static
+cp -r static /Users/sherry/Documents/program-project/website/leaprosnow-static
+cp -r media /Users/sherry/Documents/program-project/website/leaprosnow-static
 #mkdir certification
 #mv build/Leapro-snow/certification certification/index.html
 #mv certification Leapro-snow/
